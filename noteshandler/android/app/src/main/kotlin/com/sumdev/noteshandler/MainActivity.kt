d1c2569b07b31d45bc68afc51dd5413976507f33
sumandevkota@gmail.com <sumandevkota75@gmail.com>
2023-09-15 10:57:49 +0545

    appbar title and login button added with no functionality

--- noteshandler/android/app/src/main/kotlin/com/sumdev/noteshandler/MainActivity.kt
+++ noteshandler/android/app/src/main/kotlin/com/sumdev/noteshandler/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.sumdev.noteshandler
-
+import java.util.Properties
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
--- noteshandler/test/widget_test.dart
+++ noteshandler/test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:noteshandler/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
